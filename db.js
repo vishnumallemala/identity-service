43defc05b9751420158f9d91d8599ac501a499c8
Srivishnu Mallemala <vishnumallemala3@gmail.com>
2024-06-05 12:17:29 +0530

    changes in db.js

--- db.js
+++ db.js
@@ -1,13 +1,20 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
-const caCert = fs.readFileSync('./ca.pem');
 // Create a new instance of Sequelize, passing the connection parameters
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
+
+let options = {};
+
+if (process.env.DB_HOST === 'localhost') {
+  options = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    logging: false, // Disable logging or set it to console.log for detailed logs
+  };
+} else {
+  const caCert = fs.readFileSync('./ca.pem');
+  options = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
@@ -18,7 +25,14 @@ const sequelize = new Sequelize(
       },
     },
     logging: false, // Disable logging or set it to console.log for detailed logs
-  }
+  };
+}
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  options
 );
 
 // Test the connection
